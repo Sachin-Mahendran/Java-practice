fd6ce432f285ac6ee18bebb1ebf9fb150db10d78
Sachin-Mahendran <sachinmahendran62360@gmail.com>
2025-12-12 09:51:10 +0530

    Delete day 2(operators) directory

--- day 2(operators)/areaofcircle.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class areaofcircle {
-    public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        double r=s.nextDouble();
-        final double pi=3.14;
-        System.out.println("area of circle->"+pi*(r*r));
-        }
-}
--- day 2(operators)/lastdigitofnum.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class lastdigitofnum {
-    public static void main(String []args)
-    {
-        Scanner s=new Scanner(System.in);
-        int a=s.nextInt();
-        System.out.println(a%10);
-    }
-}
